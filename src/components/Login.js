5bf9c2d91486548e5639ed10f59e0be3d4a39916
HollyJHuber <hollyjhuber@gmail.com>
2017-11-21 09:48:05 -0500

    add components, input form fields

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className = "Login">
+        <form onSubmit = {this.handleSubmit}>
+          <h2> SIGN IN </h2>
+          <input  type = "text"
+                  name = "username" />
+          <br />
+          <input  type = "password"
+                  name = "password" />
+          <br />
+          <button type = "submit">
+          Sign In </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+}
+
+
+export default Login;
